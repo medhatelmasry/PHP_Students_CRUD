37bb993c26914c1ff06b74ee2890c5f2ad943247
Helen <howareyouhelen@hotmail.com>
2021-02-13 16:24:33 -0800

    removed creation files and put auto db table and data in an inc file that runs at start automatically

--- src/index.php
+++ src/index.php
@@ -1,8 +1,8 @@
 <?php include("inc_header.php"); ?>
+<?php include("inc_auto_db.php"); ?>
 
 <h1>CRUD Operations with PHP &amp; MySQL</h1>
 <div style="font-size: x-large;">
-<p><a href="./create_db">Auto DB, table & sample data</a></p>
 <p><a href="./crud/list">List Students (CRUD)</a></p>
 <p><a href="./crud/injection/injection.php">SQL Injection Example</a></p>
 </div>
