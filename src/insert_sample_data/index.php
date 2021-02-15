167366ac6379be4e70070f47d3459a514843e27f
oves98 <oves.patel98@gmail.com>
2021-02-15 12:15:28 -0800

    Finsihed the 9th issues - Oves and Cuong

--- src/insert_sample_data/index.php
+++ src/insert_sample_data/index.php
@@ -7,21 +7,34 @@ if (isset($_POST['Submit'])) {
     include("../inc_db_params.php");
     mysqli_select_db($conn, $db_name);
 
-    if ($conn !== FALSE) {
+$row = 1;
+if (($handle = fopen("../test.csv", "r")) !== FALSE) {
+    $data = fgetcsv($handle, 1000, ",");
+    while (($data = fgetcsv($handle, 1000, ",")) !== FALSE) {
+        
+        $num = count($data);
+        echo "<p> $num fields in line $row: <br /></p>\n";
+        $row++;
+        // for ($c=0; $c < $num; $c++) {     
+        //     echo $data[$c] . "<br />\n";     
+        // }
+
+        $id = mysqli_real_escape_string($conn, $data[0]);
+        $firstName = mysqli_real_escape_string($conn, $data[1]);
+        $lastName = mysqli_real_escape_string($conn, $data[2]);
+        $school = mysqli_real_escape_string($conn, $data[3]);
+
         $SQLstring = "INSERT INTO Students (StudentId, FirstName, LastName, School) 
         VALUES 
-        ('A00111111', 'Tom', 'Max', 'Science'),
-        ('A00222222', 'Ann', 'Fay', 'Mining'),
-        ('A00333333', 'Joe', 'Sun', 'Nursing'),
-        ('A00444444', 'Sue', 'Fox', 'Computing'),
-        ('A00555555', 'Ben', 'Ray', 'Mining')
+        ('$id', '$firstName', '$lastName', '$school')
         ";
         $QueryResult = mysqli_query($conn, $SQLstring);
+        echo $SQLstring;
         $rowcount=mysqli_affected_rows($conn);
         printf("<p>%d records were inserted.</p>\n", $rowcount);
 
-        $conn->close();
     }
+}
 }
 ?>
 <form action="/insert_sample_data/index.php" method="POST">
@@ -30,3 +43,5 @@ if (isset($_POST['Submit'])) {
 <a href="/" class="btn btn-small btn-primary">&lt;&lt; BACK</a>
 
 <?php include("../inc_footer.php"); ?>
+
+
