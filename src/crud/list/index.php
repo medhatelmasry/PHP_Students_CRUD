657d3a712e138110ee1e963535979ff84dbcdb1d
Michael Lin <aznshademan@gmail.com>
2021-02-15 21:09:48 -0800

    Added pie graph, hard coded rather than dynamic

--- src/crud/list/index.php
+++ src/crud/list/index.php
@@ -10,6 +10,11 @@
 include("../../inc_db_params.php");
 mysqli_select_db($conn, $db_name);
 
+$science = 0;
+$mining = 0;
+$nursing = 0;
+$computing = 0;
+
 if ($conn !== FALSE) {
     $SQLstring = "select * from Students";
     if ($QueryResult = @mysqli_query($conn, $SQLstring)) {
@@ -31,6 +36,16 @@ if ($conn !== FALSE) {
              echo "&nbsp;";
              echo "<a class='btn btn-small btn-warning' href='../../crud/update/update.php?id={$Row[0]}'>upd</a>";
              echo "</td></tr>\n";
+
+             if ($Row[3] == "Mining") {
+                  $mining += 1;
+             } else if ($Row[3] == "Science") {
+                  $science += 1;
+             } else if ($Row[3] == "Computing") {
+                  $computing += 1;
+             } else if ($Row[3] == "Nursing") {
+                  $nursing += 1;
+             }
         };
         echo "</table>\n";
 
@@ -40,12 +55,56 @@ if ($conn !== FALSE) {
              . " columns.</p>";
 
         mysqli_free_result($QueryResult);
+
+        $dataPoints = array( 
+          array("label"=>"Science", "y"=>$science),
+          array("label"=>"Mining", "y"=>$mining),
+          array("label"=>"Nursing", "y"=>$nursing),
+          array("label"=>"Computing", "y"=>$computing),
+          );
+        
+     
    }
 
    mysqli_close($conn);
 }
 
+
+
 ?>
+?>
+<!DOCTYPE HTML>
+<html>
+<head>
+<script>
+window.onload = function() {
+ 
+ 
+var chart = new CanvasJS.Chart("chartContainer", {
+	animationEnabled: true,
+	title: {
+		text: "Usage Share of Desktop Browsers"
+	},
+	subtitles: [{
+		text: "November 2017"
+	}],
+	data: [{
+		type: "pie",
+		yValueFormatString: "#,##0.00\"\"",
+		indexLabel: "{label} ({y})",
+		dataPoints: <?php echo json_encode($dataPoints, JSON_NUMERIC_CHECK); ?>
+	}]
+});
+chart.render();
+ 
+}
+</script>
+</head>
+<body>
+<div id="chartContainer" style="height: 370px; width: 100%;"></div>
+<script src="https://canvasjs.com/assets/script/canvasjs.min.js"></script>
+</body>
+</html>      
 <a href="/" class="btn btn-small btn-primary">&lt;&lt; BACK</a>
 
 <?php include("../../inc_footer.php"); ?>
